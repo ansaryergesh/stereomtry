0b938a1e89db05db338d82831674c5fe6bb16e26
ansaryergesh <yergeshovansar@gmail.com>
2020-05-28 00:04:20 +0600

    changes

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.css';
 import 'bootstrap-social/bootstrap-social.css';
 
-import * as serviceWorker from './serviceWorker';
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
